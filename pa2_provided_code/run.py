595650e9655960cb8965fa9ba8cfae92086e1747
luckyharryji <xiangyu.ji@outlook.com>
2016-05-01 15:35:58 -0500

    add run.py finish

--- pa2_provided_code/run.py
+++ pa2_provided_code/run.py
@@ -3,7 +3,7 @@ from MancalaGUI import *
 import xjq158
 
 # player1 = Player(1, Player.ABPRUNE, 10)
-player1 = xjq158.xjq158(1, xjq158.Player.HUMAN)
+player1 = xjq158.xjq158(1, xjq158.Player.ABPRUNE, 5)
 
 player2 = xjq158.xjq158(2, xjq158.Player.CUSTOM)
 
