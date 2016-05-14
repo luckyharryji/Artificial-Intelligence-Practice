51afd84547bcb08cbf49aa59a9284e82c4c3149d
luckyharryji <xiangyu.ji@outlook.com>
2016-05-14 14:06:19 -0500

    finish baseline

--- provided_code_and_data/bayes.py
+++ provided_code_and_data/bayes.py
@@ -14,12 +14,16 @@ class Bayes_Classifier:
         is ready to classify input text."""
         self.positive_hash = dict()
         self.negative_hash = dict()
+        self.word_list = list()
+        self.count = [0, 0]
         self.try_load_pickle()
 
     def try_load_pickle(self):
         try:
             self.positive_hash = self.load('positive_word_count')
             self.negative_hash = self.load('negative_word_count')
+            self.count = self.load('data_num_count')
+            self.word_list = self.load('word_list')
         except:
             self.positive_hash, self.negative_hash = self.train()
 
@@ -32,21 +36,56 @@ class Bayes_Classifier:
             if file_name[:5] == 'movie':
                 raw_text = self.loadFile('movies_reviews/' + file_name)
                 token_list = self.tokenize(raw_text)
-                if file_name[6] == '5':
+                if file_name[7] == '5':
+                    self.count[0] += 1
                     for token in token_list:
+                        if token not in self.word_list:
+                            self.word_list.append(token)
                         self.positive_hash[token.lower()] = self.positive_hash.get(token.lower(), 0) + 1
                 else:
+                    self.count[1] += 1
                     for token in token_list:
+                        if token not in self.word_list:
+                            self.word_list.append(token)
                         self.negative_hash[token.lower()] = self.negative_hash.get(token.lower(), 0) + 1
+        total_word_num = len(self.word_list)
+        for key in self.positive_hash:
+            self.positive_hash[key] = (float(self.positive_hash[key]) + float(1)) / (float(float(self.positive_hash[key]) + float(self.negative_hash.get(key, 0))) + float(total_word_num))
+        for key in self.negative_hash:
+            self.negative_hash[key] = (float(self.negative_hash[key]) + float(1)) / (float(float(self.negative_hash[key]) + float(self.positive_hash.get(key, 0))) + float(total_word_num))
         self.save(self.positive_hash, 'positive_word_count')
         self.save(self.negative_hash, 'negative_word_count')
+        self.save(self.count, 'data_num_count')
+        self.save(self.word_list, 'word_list')
         return self.positive_hash, self.negative_hash
 
     def classify(self, sText):
         """Given a target string sText, this function returns the most likely document
         class to which the target string belongs (i.e., positive, negative or neutral).
         """
-        pass
+        input_token = self.tokenize(sText)
+        num_of_data = sum(self.count)
+        num_of_word = len(self.word_list)
+        p_positive, p_negative = math.log(float(self.count[0]) / float(num_of_data)), math.log(float(self.count[1]) / float(num_of_data))
+        # print self.count[0], self.count[1]
+        # print num_of_data
+        # p_positive, p_negative = math.log(float(self.count[0]) / float(num_of_data)), 0
+        for word_token in input_token:
+            word = word_token.lower()
+            if self.positive_hash.get(word):
+                p_positive += math.log(self.positive_hash.get(word))
+            else:
+                p_positive += math.log(float(1) / float(num_of_word))
+            if self.negative_hash.get(word):
+                p_negative += math.log(self.negative_hash.get(word))
+            else:
+                p_negative += math.log(float(1) / float(num_of_word))
+        if p_positive > p_negative:
+            return 'Positive'
+        elif p_positive < p_negative:
+            return 'Negative'
+        else:
+            return 'Neural'
 
     def loadFile(self, sFilename):
         """Given a file name, return the contents of the file as a string."""
@@ -94,4 +133,5 @@ class Bayes_Classifier:
 
 if __name__ == '__main__':
     test_obj = Bayes_Classifier()
+    print test_obj.classify('I love AI class')
     # positive_set, negative_set = test_obj.train()
